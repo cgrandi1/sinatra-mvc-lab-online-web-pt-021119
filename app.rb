1eb5af52153e8118320327a15ce19152cfc63be6
Carlton Dwayne <carltondgrandison@gmail.com>
2019-05-30 23:32:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+
+    erb :user_input
+  end
+
+  get '/piglatinize' do
+
+    erb :user_input
+  end
+  post '/' do
+
+    @params = params
+    binding.pry
+    erb :user_input
+  end
+
 end
