8be20706243d43a6f2da1a6484a08bcfd91468af
Carlton Dwayne <carltondgrandison@gmail.com>
2019-05-31 01:07:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
   post '/piglatinize' do
     phrase = params["user_phrase"]
     pig = PigLatinzer.new
-    @newphrase = pig.piglatinizer(phrase)
+    @newphrase = pig.piglatinize(phrase)
     binding.pry
     #params => {"user_phrase" => "whatever you type in in the form"}
 
